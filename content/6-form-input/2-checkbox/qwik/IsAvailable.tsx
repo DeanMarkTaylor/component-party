0a66d9060f270a8731dc5652d15076d3bf2e08ef
Dean Taylor <deanmarktaylor@gmail.com>
2023-05-16 02:53:50 +0100

    Update Qwik IsAvailable.tsx to simplify
    Use bind attribute and `useSignal` to match Svelte example:
    https://qwik.builder.io/docs/components/rendering/#bind-attribute

--- content/6-form-input/2-checkbox/qwik/IsAvailable.tsx
+++ content/6-form-input/2-checkbox/qwik/IsAvailable.tsx
@@ -1,23 +1,13 @@
-import { component$, useStore, $ } from "@builder.io/qwik";
+import { component$, useSignal, $ } from "@builder.io/qwik";
 
-const IsAvailable = component$(() => {
-  const store = useStore({ isAvailable: false });
-
-  const handleChange = $(() => {
-    store.isAvailable = !store.isAvailable;
-  });
+export default component$(() => {
+  const isAvailable = useSignal(false);
 
   return (
     <>
-      <input
-        id="is-available"
-        type="checkbox"
-        checked={store.isAvailable}
-        onChange$={handleChange}
-      />
+      <input id="is-available" type="checkbox" bind:checked={isAvailable} />
       <label for="is-available">Is available</label>
     </>
   );
 });
 
-export default IsAvailable;
